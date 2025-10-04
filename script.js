76db9eac6d1d29f01a9761af6c985117fc332f5a
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-10-04 12:31:30 +0530

    Created all the variables and the newGame/Restart Function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
   and the turn changes to other person. 
 */
 
-const diceImage = [
+const diceImagePath = [
   'dice-1.png',
   'dice-2.png',
   'dice-3.png',
@@ -18,16 +18,38 @@ const diceImage = [
   'dice-5.png',
   'dice-6.png',
 ];
-const dice = document.querySelector('.dice');
+
+const diceImage = document.querySelector('.dice');
+const diceRollBtn = document.querySelector('.btn--roll');
+const newGame = document.querySelector('.btn--new');
+const holdBtn = document.querySelector('.btn--hold');
+
+// Player 1
+const player1 = document.querySelector('.player--0'); // Section
+const player1Score = document.getElementById('score--0'); // Score to be saved when Hold Button is pressed
+const player1CurrentScore = document.getElementById('current--0'); // Score when Dice Roll Button is pressed
+
+// Player 2
+const player2 = document.querySelector('.player--1'); // Section
+const player2Score = document.getElementById('score--1'); // Score to be saved when Hold Button is pressed
+const player2CurrentScore = document.getElementById('current--1'); // Score when Dice Roll Button is pressed
+let playerTurn = false; // false = player 1 & true = player 2
 
 const diceRoll = () => {
   const randomRoll = Math.trunc(Math.random() * 6) + 1;
-  dice.src = diceImage[randomRoll - 1];
+  diceImage.src = diceImagePath[randomRoll - 1];
+  diceImage.style.opacity = '1';
   return randomRoll;
 };
 
-let playerTurn = false; // false = player 1 & true = player 2
-
 const restart = function () {
-  //Happens when the New Game is Pressed
+  diceImage.style.opacity = '0';
+  player1Score.innerHTML = 0;
+  player1CurrentScore.innerHTML = 0;
+  player2Score.innerHTML = 0;
+  player2CurrentScore.innerHTML = 0;
 };
+
+restart();
+
+newGame.addEventListener('click', restart);
